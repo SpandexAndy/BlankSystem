daa8b1fda792536ff1ae3a0555146c53d0ea7cfd
SpandexAndy <andy@spandexandy.net>
2022-02-28 19:31:23 +0200

    Basic actor sheet.

--- system/BlankSystem.js
+++ system/BlankSystem.js
@@ -2,3 +2,15 @@
 
 // Sheets
 import PlayerSheet from "./sheets/PlayerSheet.js"; 
+
+// Create a function that will run on startup. (Blank for now).
+Hooks.once("init"", function() {
+   // You can display something into chat
+   console.log("BlankSystem | Message of the Day!");
+   
+   // Deregister the default sheet so users can't choose it
+   Actors.unregisterSheet("core", ActorSheet);
+   
+   // Register the sheets we will use, and pick our default one.
+   Actors.registerSheet("BlankSystem", PlayerSheet, {makeDefault: true});
+});
